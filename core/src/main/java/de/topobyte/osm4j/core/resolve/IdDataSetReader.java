bb0c7e142d5ccda0fbe0353b1f5d483136cd937a
Sebastian Kürten <sebastian@topobyte.de>
2015-10-30 12:59:58 +0100

    Id-only utilities for the 'resolve' package
    * Added an InMemoryIdDataSet for storing a set of osm ids
    * Added the IdDataSetReader that builds an InMemoryIdDataSet from an
      OsmIdIterator, OsmIterator, OsmIdReader or OsmReader

---
+++ core/src/main/java/de/topobyte/osm4j/core/resolve/IdDataSetReader.java
@@ -0,0 +1,199 @@
+// Copyright 2015 Sebastian Kuerten
+//
+// This file is part of osm4j.
+//
+// osm4j is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osm4j is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with osm4j. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.osm4j.core.resolve;
+
+import gnu.trove.set.TLongSet;
+
+import java.io.IOException;
+
+import de.topobyte.osm4j.core.access.OsmHandler;
+import de.topobyte.osm4j.core.access.OsmIdHandler;
+import de.topobyte.osm4j.core.access.OsmIdIterator;
+import de.topobyte.osm4j.core.access.OsmIdReader;
+import de.topobyte.osm4j.core.access.OsmInputException;
+import de.topobyte.osm4j.core.access.OsmIterator;
+import de.topobyte.osm4j.core.access.OsmReader;
+import de.topobyte.osm4j.core.model.iface.EntityContainer;
+import de.topobyte.osm4j.core.model.iface.IdContainer;
+import de.topobyte.osm4j.core.model.iface.OsmBounds;
+import de.topobyte.osm4j.core.model.iface.OsmNode;
+import de.topobyte.osm4j.core.model.iface.OsmRelation;
+import de.topobyte.osm4j.core.model.iface.OsmWay;
+
+public class IdDataSetReader
+{
+
+	public static InMemoryIdDataSet read(OsmIdIterator iterator)
+			throws IOException
+	{
+		InMemoryIdDataSet dataSet = new InMemoryIdDataSet();
+
+		TLongSet nodeIds = dataSet.getNodeIds();
+		TLongSet wayIds = dataSet.getWayIds();
+		TLongSet relationIds = dataSet.getRelationIds();
+
+		if (iterator.hasBounds()) {
+			dataSet.setBounds(iterator.getBounds());
+		}
+
+		while (iterator.hasNext()) {
+			IdContainer container = iterator.next();
+			switch (container.getType()) {
+			case Node:
+				nodeIds.add(container.getId());
+				break;
+			case Way:
+				wayIds.add(container.getId());
+				break;
+			case Relation:
+				relationIds.add(container.getId());
+				break;
+			}
+		}
+
+		return dataSet;
+	}
+
+	public static InMemoryIdDataSet read(OsmIterator iterator)
+			throws IOException
+	{
+		InMemoryIdDataSet dataSet = new InMemoryIdDataSet();
+
+		TLongSet nodeIds = dataSet.getNodeIds();
+		TLongSet wayIds = dataSet.getWayIds();
+		TLongSet relationIds = dataSet.getRelationIds();
+
+		if (iterator.hasBounds()) {
+			dataSet.setBounds(iterator.getBounds());
+		}
+
+		while (iterator.hasNext()) {
+			EntityContainer container = iterator.next();
+			switch (container.getType()) {
+			case Node:
+				nodeIds.add(container.getEntity().getId());
+				break;
+			case Way:
+				wayIds.add(container.getEntity().getId());
+				break;
+			case Relation:
+				relationIds.add(container.getEntity().getId());
+				break;
+			}
+		}
+
+		return dataSet;
+	}
+
+	public static InMemoryIdDataSet read(OsmIdReader reader)
+			throws OsmInputException
+	{
+		final InMemoryIdDataSet dataSet = new InMemoryIdDataSet();
+
+		final TLongSet nodeIds = dataSet.getNodeIds();
+		final TLongSet wayIds = dataSet.getWayIds();
+		final TLongSet relationIds = dataSet.getRelationIds();
+
+		reader.setIdHandler(new OsmIdHandler() {
+
+			@Override
+			public void handle(OsmBounds bounds) throws IOException
+			{
+				dataSet.setBounds(bounds);
+			}
+
+			@Override
+			public void handleNode(long id) throws IOException
+			{
+				nodeIds.add(id);
+			}
+
+			@Override
+			public void handleWay(long id) throws IOException
+			{
+				wayIds.add(id);
+			}
+
+			@Override
+			public void handleRelation(long id) throws IOException
+			{
+				relationIds.add(id);
+			}
+
+			@Override
+			public void complete() throws IOException
+			{
+				// nothing to do here
+			}
+
+		});
+
+		reader.read();
+
+		return dataSet;
+	}
+
+	public static InMemoryIdDataSet read(OsmReader reader)
+			throws OsmInputException
+	{
+		final InMemoryIdDataSet dataSet = new InMemoryIdDataSet();
+
+		final TLongSet nodeIds = dataSet.getNodeIds();
+		final TLongSet wayIds = dataSet.getWayIds();
+		final TLongSet relationIds = dataSet.getRelationIds();
+
+		reader.setHandler(new OsmHandler() {
+
+			@Override
+			public void handle(OsmBounds bounds) throws IOException
+			{
+				dataSet.setBounds(bounds);
+			}
+
+			@Override
+			public void handle(OsmNode node) throws IOException
+			{
+				nodeIds.add(node.getId());
+			}
+
+			@Override
+			public void handle(OsmWay way) throws IOException
+			{
+				wayIds.add(way.getId());
+			}
+
+			@Override
+			public void handle(OsmRelation relation) throws IOException
+			{
+				relationIds.add(relation.getId());
+			}
+
+			@Override
+			public void complete() throws IOException
+			{
+				// nothing to do here
+			}
+
+		});
+
+		reader.read();
+
+		return dataSet;
+	}
+
+}
---
+++ core/src/main/java/de/topobyte/osm4j/core/resolve/InMemoryIdDataSet.java
@@ -0,0 +1,104 @@
+// Copyright 2015 Sebastian Kuerten
+//
+// This file is part of osm4j.
+//
+// osm4j is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osm4j is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with osm4j. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.osm4j.core.resolve;
+
+import gnu.trove.set.TLongSet;
+import gnu.trove.set.hash.TLongHashSet;
+import de.topobyte.osm4j.core.model.iface.OsmBounds;
+
+/**
+ * A structure representing the ids of elements found in a set of osm-data.
+ * 
+ * @author Sebastian Kuerten (sebastian@topobyte.de)
+ */
+public class InMemoryIdDataSet
+{
+
+	private OsmBounds bounds = null;
+
+	private TLongSet nodeIds = new TLongHashSet();
+	private TLongSet wayIds = new TLongHashSet();
+	private TLongSet relationIds = new TLongHashSet();
+
+	public boolean hasBounds()
+	{
+		return bounds != null;
+	}
+
+	public OsmBounds getBounds()
+	{
+		return bounds;
+	}
+
+	public void setBounds(OsmBounds bounds)
+	{
+		this.bounds = bounds;
+	}
+
+	/**
+	 * @return all node ids.
+	 */
+	public TLongSet getNodeIds()
+	{
+		return nodeIds;
+	}
+
+	/**
+	 * @return all way ids.
+	 */
+	public TLongSet getWayIds()
+	{
+		return wayIds;
+	}
+
+	/**
+	 * @return all relation ids.
+	 */
+	public TLongSet getRelationIds()
+	{
+		return relationIds;
+	}
+
+	/**
+	 * @param nodeIds
+	 *            set the node ids of this dataset to be these.
+	 */
+	public void setNodeIds(TLongSet nodeIds)
+	{
+		this.nodeIds = nodeIds;
+	}
+
+	/**
+	 * @param wayIds
+	 *            set the way ids of this dataset to be these.
+	 */
+	public void setWayIds(TLongSet wayIds)
+	{
+		this.wayIds = wayIds;
+	}
+
+	/**
+	 * @param relationIds
+	 *            set the relation ids of this dataset to be these.
+	 */
+	public void setRelationIds(TLongSet relationIds)
+	{
+		this.relationIds = relationIds;
+	}
+
+}
