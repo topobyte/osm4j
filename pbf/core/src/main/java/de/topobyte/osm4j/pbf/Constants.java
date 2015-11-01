35f06a6a98d94585d76f5f8e13d5ca15bbe8896c
Sebastian Kürten <sebastian@topobyte.de>
2015-11-01 10:48:13 +0100

    Updated 'writing program' property

--- pbf/core/src/main/java/de/topobyte/osm4j/pbf/Constants.java
+++ pbf/core/src/main/java/de/topobyte/osm4j/pbf/Constants.java
@@ -23,7 +23,7 @@ public class Constants
 	public static final String BLOCK_TYPE_HEADER = "OSMHeader";
 	public static final String BLOCK_TYPE_DATA = "OSMData";
 
-	public static final String WRITING_PROGRAM = "osm4j-pbf-0.0.1";
+	public static final String WRITING_PROGRAM = "osm4j-pbf-0.0.4";
 
 	public static final String FEATURE_SCHEMA_0_6 = "OsmSchema-V0.6";
 	public static final String FEATURE_DENSE_NODES = "DenseNodes";
