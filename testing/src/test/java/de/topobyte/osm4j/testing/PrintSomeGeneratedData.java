56f46a0287e9de70d7aa124ddba62bd84746f3e0
Sebastian Kürten <sebastian@topobyte.de>
2015-11-01 01:17:23 +0100

    Added a copyright header

--- testing/src/test/java/de/topobyte/osm4j/testing/PrintSomeGeneratedData.java
+++ testing/src/test/java/de/topobyte/osm4j/testing/PrintSomeGeneratedData.java
@@ -1,12 +1,25 @@
+// Copyright 2015 Sebastian Kuerten
+//
+// This file is part of osm4j.
+//
+// osm4j is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osm4j is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with osm4j. If not, see <http://www.gnu.org/licenses/>.
+
 package de.topobyte.osm4j.testing;
 
 import java.io.IOException;
 
 import de.topobyte.osm4j.core.access.OsmOutputStream;
-import de.topobyte.osm4j.testing.DataSet;
-import de.topobyte.osm4j.testing.DataSetGenerator;
-import de.topobyte.osm4j.testing.DataSetHelper;
-import de.topobyte.osm4j.testing.EntityGenerator;
 import de.topobyte.osm4j.xml.output.OsmXmlOutputStream;
 
 public class PrintSomeGeneratedData
