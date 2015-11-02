be01312c4739749db42e58a098bb1bc772ea49ae
Sebastian Kürten <sebastian@topobyte.de>
2015-11-02 23:41:02 +0100

    Added the OsmIoUtils class
    * Helper methods for setting up an OsmIterator or OsmReader for
      specified InputStream and FileFormat
    * Helper method for setting up an OsmOutputStream for specified
      OutputStream and FileFormat
    * Helper method for getting the default extension for the different
      file formats

---
+++ utils/src/main/java/de/topobyte/osm4j/utils/OsmIoUtils.java
@@ -0,0 +1,104 @@
+// Copyright 2015 Sebastian Kuerten
+//
+// This file is part of osm4j.
+//
+// osm4j is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osm4j is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with osm4j. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.osm4j.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import de.topobyte.osm4j.core.access.OsmIterator;
+import de.topobyte.osm4j.core.access.OsmOutputStream;
+import de.topobyte.osm4j.core.access.OsmReader;
+import de.topobyte.osm4j.pbf.seq.PbfIterator;
+import de.topobyte.osm4j.pbf.seq.PbfReader;
+import de.topobyte.osm4j.pbf.seq.PbfWriter;
+import de.topobyte.osm4j.tbo.access.TboIterator;
+import de.topobyte.osm4j.tbo.access.TboReader;
+import de.topobyte.osm4j.tbo.access.TboWriter;
+import de.topobyte.osm4j.utils.config.PbfConfig;
+import de.topobyte.osm4j.utils.config.TboConfig;
+import de.topobyte.osm4j.xml.dynsax.OsmXmlIterator;
+import de.topobyte.osm4j.xml.dynsax.OsmXmlReader;
+import de.topobyte.osm4j.xml.output.OsmXmlOutputStream;
+
+public class OsmIoUtils
+{
+
+	public static OsmIterator setupOsmIterator(InputStream in,
+			FileFormat format, boolean readMetadata) throws IOException
+	{
+		switch (format) {
+		default:
+		case PBF:
+			return new PbfIterator(in, readMetadata);
+		case TBO:
+			return new TboIterator(in, readMetadata);
+		case XML:
+			return new OsmXmlIterator(in, readMetadata);
+		}
+	}
+
+	public static OsmReader setupOsmReader(InputStream in, FileFormat format,
+			boolean readMetadata) throws IOException
+	{
+		switch (format) {
+		default:
+		case PBF:
+			return new PbfReader(in, readMetadata);
+		case TBO:
+			return new TboReader(in, readMetadata);
+		case XML:
+			return new OsmXmlReader(in, readMetadata);
+		}
+	}
+
+	public static OsmOutputStream setupOsmOutput(OutputStream out,
+			FileFormat format, boolean writeMetadata, PbfConfig pbfConfig,
+			TboConfig tboConfig)
+	{
+		switch (format) {
+		default:
+		case PBF:
+			PbfWriter pbfWriter = new PbfWriter(out, writeMetadata);
+			pbfWriter.setCompression(pbfConfig.getCompression());
+			pbfWriter.setUseDense(pbfConfig.isUseDenseNodes());
+			return pbfWriter;
+		case TBO:
+			TboWriter tboWriter = new TboWriter(out, writeMetadata, true);
+			tboWriter.setCompression(tboConfig.getCompression());
+			return tboWriter;
+		case XML:
+			return new OsmXmlOutputStream(out, writeMetadata);
+		}
+	}
+
+	public static String extension(FileFormat format)
+	{
+		switch (format) {
+		default:
+			return null;
+		case PBF:
+			return ".pbf";
+		case TBO:
+			return ".tbo";
+		case XML:
+			return ".xml";
+		}
+	}
+
+}
