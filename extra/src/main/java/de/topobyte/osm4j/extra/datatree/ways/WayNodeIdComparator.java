a39cdb6ce8d627f42ca1df399f10ff1c9a2cccc7
Sebastian Kürten <sebastian@topobyte.de>
2015-11-10 18:44:33 +0100

    WayNodeIdComparator: sort by first node id, then way id

--- extra/src/main/java/de/topobyte/osm4j/extra/datatree/ways/WayNodeIdComparator.java
+++ extra/src/main/java/de/topobyte/osm4j/extra/datatree/ways/WayNodeIdComparator.java
@@ -27,7 +27,13 @@ public class WayNodeIdComparator implements Comparator<OsmWay>
 	@Override
 	public int compare(OsmWay o1, OsmWay o2)
 	{
-		return Long.compare(o1.getNodeId(0), o2.getNodeId(0));
+		long id1 = o1.getNodeId(0);
+		long id2 = o2.getNodeId(0);
+		int cmp = Long.compare(id1, id2);
+		if (cmp != 0) {
+			return cmp;
+		}
+		return Long.compare(o1.getId(), o2.getId());
 	}
 
 }
