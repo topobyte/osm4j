4cc9af677084e2e797beac606097a57c7b229454
Sebastian Kürten <sebastian@topobyte.de>
2015-11-11 13:16:46 +0100

    Abstract task with CLI option for input and output

---
+++ utils/src/main/java/de/topobyte/osm4j/utils/AbstractTaskInputOutput.java
@@ -0,0 +1,77 @@
+// Copyright 2015 Sebastian Kuerten
+//
+// This file is part of osm4j.
+//
+// osm4j is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osm4j is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with osm4j. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.osm4j.utils;
+
+import de.topobyte.osm4j.utils.config.PbfConfig;
+import de.topobyte.osm4j.utils.config.PbfOptions;
+import de.topobyte.osm4j.utils.config.TboConfig;
+import de.topobyte.osm4j.utils.config.TboOptions;
+import de.topobyte.utilities.apache.commons.cli.OptionHelper;
+
+public abstract class AbstractTaskInputOutput extends AbstractTask
+{
+
+	private static final String OPTION_INPUT_FORMAT = "input_format";
+	private static final String OPTION_OUTPUT_FORMAT = "output_format";
+
+	protected FileFormat inputFormat;
+	protected FileFormat outputFormat;
+	protected PbfConfig pbfConfig;
+	protected TboConfig tboConfig;
+
+	protected boolean readMetadata = true;
+	protected boolean writeMetadata = true;
+
+	public AbstractTaskInputOutput()
+	{
+		// @formatter:off
+		OptionHelper.add(options, OPTION_INPUT_FORMAT, true, true, "the file format of the input");
+		OptionHelper.add(options, OPTION_OUTPUT_FORMAT, true, true, "the file format of the output");
+		PbfOptions.add(options);
+		TboOptions.add(options);
+		// @formatter:on
+	}
+
+	@Override
+	protected void setup(String[] args)
+	{
+		super.setup(args);
+
+		String inputFormatName = line.getOptionValue(OPTION_INPUT_FORMAT);
+		inputFormat = FileFormat.parseFileFormat(inputFormatName);
+		if (inputFormat == null) {
+			System.out.println("invalid input format");
+			System.out.println("please specify one of: "
+					+ FileFormat.getHumanReadableListOfSupportedFormats());
+			System.exit(1);
+		}
+
+		String outputFormatName = line.getOptionValue(OPTION_OUTPUT_FORMAT);
+		outputFormat = FileFormat.parseFileFormat(outputFormatName);
+		if (outputFormat == null) {
+			System.out.println("invalid output format");
+			System.out.println("please specify one of: "
+					+ FileFormat.getHumanReadableListOfSupportedFormats());
+			System.exit(1);
+		}
+
+		pbfConfig = PbfOptions.parse(line);
+		tboConfig = TboOptions.parse(line);
+	}
+
+}
