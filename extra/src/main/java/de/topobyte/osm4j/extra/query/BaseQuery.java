74a5f6db6dca0ab3b2ddf19943780eadd9bad322
Sebastian Kürten <sebastian@topobyte.de>
2015-11-17 11:01:16 +0100

    Prepared bounding box queries to data structures

---
+++ extra/src/main/java/de/topobyte/osm4j/extra/query/BaseQuery.java
@@ -0,0 +1,111 @@
+// Copyright 2015 Sebastian Kuerten
+//
+// This file is part of osm4j.
+//
+// osm4j is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osm4j is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with osm4j. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.osm4j.extra.query;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.vividsolutions.jts.geom.Envelope;
+
+import de.topobyte.jts.utils.predicate.ContainmentTest;
+import de.topobyte.osm4j.extra.datatree.DataTree;
+import de.topobyte.osm4j.utils.AbstractExecutableInputOutput;
+import de.topobyte.utilities.apache.commons.cli.OptionHelper;
+
+public abstract class BaseQuery extends AbstractExecutableInputOutput
+{
+
+	private static final String OPTION_OUTPUT = "output";
+	private static final String OPTION_TREE = "tree";
+	private static final String OPTION_SIMPLE_RELATIONS = "simple_relations";
+	private static final String OPTION_COMPLEX_RELATIONS = "complex_relations";
+	private static final String OPTION_FILE_NAMES_TREE_NODES = "tree_nodes";
+	private static final String OPTION_FILE_NAMES_TREE_WAYS = "tree_ways";
+	private static final String OPTION_FILE_NAMES_TREE_RELATIONS_SIMPLE = "tree_simple_relations";
+	private static final String OPTION_FILE_NAMES_TREE_RELATIONS_COMPLEX = "tree_complex_relations";
+	private static final String OPTION_FILE_NAMES_RELATION_NODES = "relation_nodes";
+	private static final String OPTION_FILE_NAMES_RELATION_WAYS = "relation_ways";
+	private static final String OPTION_FILE_NAMES_RELATION_RELATIONS = "relation_relations";
+
+	public BaseQuery()
+	{
+		// @formatter:off
+		OptionHelper.add(options, OPTION_OUTPUT, true, true, "directory to store output in");
+		OptionHelper.add(options, OPTION_TREE, true, true, "path to the data tree");
+		OptionHelper.add(options, OPTION_SIMPLE_RELATIONS, true, true, "path to simple relation batches");
+		OptionHelper.add(options, OPTION_COMPLEX_RELATIONS, true, true, "path to complex relation batches");
+		OptionHelper.add(options, OPTION_FILE_NAMES_TREE_NODES, true, true, "name of node files in tree");
+		OptionHelper.add(options, OPTION_FILE_NAMES_TREE_WAYS, true, true, "name of way files in tree");
+		OptionHelper.add(options, OPTION_FILE_NAMES_TREE_RELATIONS_SIMPLE, true, true, "name of simple relations in tree");
+		OptionHelper.add(options, OPTION_FILE_NAMES_TREE_RELATIONS_COMPLEX, true, true, "name of complex relations in tree");
+		OptionHelper.add(options, OPTION_FILE_NAMES_RELATION_NODES, true, true, "name of node files in relation batches");
+		OptionHelper.add(options, OPTION_FILE_NAMES_RELATION_WAYS, true, true, "name of way files in relation batches");
+		OptionHelper.add(options, OPTION_FILE_NAMES_RELATION_RELATIONS, true, true, "name of relation files in relation batches");
+		// @formatter:on
+	}
+
+	protected Path pathOutput;
+	protected Path pathTree;
+	protected Path pathSimpleRelations;
+	protected Path pathComplexRelations;
+	protected DataTree tree;
+
+	protected String fileNamesTreeNodes;
+	protected String fileNamesTreeWays;
+	protected String fileNamesTreeSimpleRelations;
+	protected String fileNamesTreeComplexRelations;
+	protected String fileNamesRelationNodes;
+	protected String fileNamesRelationWays;
+	protected String fileNamesRelationRelations;
+
+	protected Envelope queryEnvelope;
+	protected ContainmentTest test;
+
+	@Override
+	protected void setup(String[] args)
+	{
+		super.setup(args);
+
+		pathOutput = Paths.get(line.getOptionValue(OPTION_OUTPUT));
+		pathTree = Paths.get(line.getOptionValue(OPTION_TREE));
+		pathSimpleRelations = Paths.get(line
+				.getOptionValue(OPTION_SIMPLE_RELATIONS));
+		pathComplexRelations = Paths.get(line
+				.getOptionValue(OPTION_COMPLEX_RELATIONS));
+
+		fileNamesTreeNodes = line.getOptionValue(OPTION_FILE_NAMES_TREE_NODES);
+		fileNamesTreeWays = line.getOptionValue(OPTION_FILE_NAMES_TREE_WAYS);
+		fileNamesTreeSimpleRelations = line
+				.getOptionValue(OPTION_FILE_NAMES_TREE_RELATIONS_SIMPLE);
+		fileNamesTreeComplexRelations = line
+				.getOptionValue(OPTION_FILE_NAMES_TREE_RELATIONS_COMPLEX);
+
+		fileNamesRelationNodes = line
+				.getOptionValue(OPTION_FILE_NAMES_RELATION_NODES);
+		fileNamesRelationWays = line
+				.getOptionValue(OPTION_FILE_NAMES_RELATION_WAYS);
+		fileNamesRelationRelations = line
+				.getOptionValue(OPTION_FILE_NAMES_RELATION_RELATIONS);
+	}
+
+	protected void execute()
+	{
+		// TODO: run query
+	}
+
+}
---
+++ extra/src/main/java/de/topobyte/osm4j/extra/query/BboxQuery.java
@@ -0,0 +1,83 @@
+// Copyright 2015 Sebastian Kuerten
+//
+// This file is part of osm4j.
+//
+// osm4j is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osm4j is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with osm4j. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.osm4j.extra.query;
+
+import java.io.IOException;
+
+import de.topobyte.adt.geo.BBox;
+import de.topobyte.adt.geo.BBoxString;
+import de.topobyte.jts.utils.predicate.ContainmentTestRectangle;
+import de.topobyte.utilities.apache.commons.cli.OptionHelper;
+
+public class BboxQuery extends BaseQuery
+{
+
+	private static final String OPTION_BBOX = "bbox";
+
+	@Override
+	protected String getHelpMessage()
+	{
+		return BaseQuery.class.getSimpleName() + " [options]";
+	}
+
+	public static void main(String[] args) throws IOException
+	{
+		BboxQuery task = new BboxQuery();
+
+		task.setup(args);
+
+		task.execute();
+	}
+
+	public BboxQuery()
+	{
+		// @formatter:off
+		OptionHelper.add(options, OPTION_BBOX, true, true, "the bbox to extract");
+		// @formatter:on
+	}
+
+	private BBox bbox;
+
+	@Override
+	protected void setup(String[] args)
+	{
+		super.setup(args);
+
+		String argBbox = line.getOptionValue(OPTION_BBOX);
+		BBoxString bboxString = BBoxString.parse(argBbox);
+		bbox = bboxString.toBbox();
+
+		if (bbox.getLat1() == 0 && bbox.getLat2() == 0 && bbox.getLon1() == 0
+				&& bbox.getLon2() == 0) {
+			System.out.println("invalid bounding box");
+			System.exit(1);
+		}
+	}
+
+	@Override
+	protected void execute()
+	{
+		queryEnvelope = bbox.toEnvelope();
+
+		test = new ContainmentTestRectangle(bbox.getLon1(), bbox.getLat2(),
+				bbox.getLon2(), bbox.getLat1());
+
+		super.execute();
+	}
+
+}
