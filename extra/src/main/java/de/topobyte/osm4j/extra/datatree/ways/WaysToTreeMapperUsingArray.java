6ce9e88fe45e703d2f4732405005772cda5eecfc
Sebastian Kürten <sebastian@topobyte.de>
2015-11-20 01:30:09 +0100

    Maed MapWaysToTreeUsingArray reusable

--- extra/src/main/java/de/topobyte/osm4j/extra/datatree/ways/WaysToTreeMapperUsingArray.java
+++ extra/src/main/java/de/topobyte/osm4j/extra/datatree/ways/WaysToTreeMapperUsingArray.java
@@ -18,11 +18,9 @@
 package de.topobyte.osm4j.extra.datatree.ways;
 
 import java.io.BufferedOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,7 +30,6 @@ import com.vividsolutions.jts.geom.Envelope;
 import de.topobyte.largescalefileio.ClosingFileOutputStreamFactory;
 import de.topobyte.largescalefileio.SimpleClosingFileOutputStreamFactory;
 import de.topobyte.osm4j.core.access.OsmIterator;
-import de.topobyte.osm4j.core.access.OsmIteratorInput;
 import de.topobyte.osm4j.core.access.OsmOutputStream;
 import de.topobyte.osm4j.core.access.OsmOutputStreamStreamOutput;
 import de.topobyte.osm4j.core.access.OsmStreamOutput;
@@ -47,57 +44,40 @@ import de.topobyte.osm4j.extra.datatree.Node;
 import de.topobyte.osm4j.extra.nodearray.NodeArray;
 import de.topobyte.osm4j.extra.nodearray.NodeArrayInteger;
 import de.topobyte.osm4j.extra.progress.NodeProgress;
-import de.topobyte.osm4j.utils.AbstractExecutableSingleInputFileOutput;
+import de.topobyte.osm4j.utils.FileFormat;
 import de.topobyte.osm4j.utils.OsmIoUtils;
 import de.topobyte.osm4j.utils.StreamUtil;
-import de.topobyte.utilities.apache.commons.cli.OptionHelper;
+import de.topobyte.osm4j.utils.config.PbfConfig;
+import de.topobyte.osm4j.utils.config.TboConfig;
 
-public class MapWaysToTreeUsingArray extends
-		AbstractExecutableSingleInputFileOutput
+public class WaysToTreeMapperUsingArray
 {
 
-	private static final String OPTION_FILE_NAMES = "filenames";
-	private static final String OPTION_TREE = "tree";
-	private static final String OPTION_NODE_ARRAY = "node_array";
+	private OsmIterator wayIterator;
 
-	@Override
-	protected String getHelpMessage()
-	{
-		return MapWaysToTreeUsingArray.class.getSimpleName() + " [options]";
-	}
-
-	public static void main(String[] args) throws IOException
-	{
-		MapWaysToTreeUsingArray task = new MapWaysToTreeUsingArray();
-
-		task.setup(args);
-
-		task.execute();
-	}
-
-	private String pathTree;
-	private String pathNodeArray;
-
+	private Path pathTree;
 	private String fileNames;
 
-	public MapWaysToTreeUsingArray()
+	private Path pathNodeArray;
+
+	private FileFormat outputFormat;
+	private PbfConfig pbfConfig;
+	private TboConfig tboConfig;
+	private boolean writeMetadata;
+
+	public WaysToTreeMapperUsingArray(OsmIterator wayIterator, Path pathTree,
+			String fileNames, Path pathNodeArray, FileFormat outputFormat,
+			PbfConfig pbfConfig, TboConfig tboConfig, boolean writeMetadata)
 	{
-		// @formatter:off
-		OptionHelper.add(options, OPTION_FILE_NAMES, true, true, "names of the data files to create");
-		OptionHelper.add(options, OPTION_TREE, true, true, "directory to store output in");
-		OptionHelper.add(options, OPTION_NODE_ARRAY, true, true, "a path to a node array");
-		// @formatter:on
-	}
+		this.wayIterator = wayIterator;
+		this.pathTree = pathTree;
+		this.fileNames = fileNames;
+		this.pathNodeArray = pathNodeArray;
 
-	@Override
-	protected void setup(String[] args)
-	{
-		super.setup(args);
-
-		fileNames = line.getOptionValue(OPTION_FILE_NAMES);
-
-		pathTree = line.getOptionValue(OPTION_TREE);
-		pathNodeArray = line.getOptionValue(OPTION_NODE_ARRAY);
+		this.outputFormat = outputFormat;
+		this.pbfConfig = pbfConfig;
+		this.tboConfig = tboConfig;
+		this.writeMetadata = writeMetadata;
 	}
 
 	public void execute() throws IOException
@@ -106,20 +86,14 @@ public class MapWaysToTreeUsingArray extends
 		 * Tree, node array and way iterator
 		 */
 
-		Path dirTree = Paths.get(pathTree);
-
-		NodeArray array = new NodeArrayInteger(new File(pathNodeArray), 1024,
+		NodeArray array = new NodeArrayInteger(pathNodeArray.toFile(), 1024,
 				4096);
-		DataTree tree = DataTreeOpener.open(dirTree.toFile());
-
-		OsmIteratorInput input = getOsmFileInput()
-				.createIterator(writeMetadata);
-		OsmIterator iterator = input.getIterator();
+		DataTree tree = DataTreeOpener.open(pathTree.toFile());
 
 		// This is where we write ways to that do not contain any reference
 		// within the world bounds
 
-		Path pathNonMatched = dirTree.resolve("non-matched-ways.tbo");
+		Path pathNonMatched = pathTree.resolve("non-matched-ways.tbo");
 		OutputStream bosNone = StreamUtil.bufferedOutputStream(pathNonMatched
 				.toFile());
 		OsmOutputStream osmOutputNone = OsmIoUtils.setupOsmOutput(bosNone,
@@ -135,7 +109,7 @@ public class MapWaysToTreeUsingArray extends
 
 		for (Node leaf : tree.getLeafs()) {
 			String dirname = Long.toHexString(leaf.getPath());
-			Path dir = dirTree.resolve(dirname);
+			Path dir = pathTree.resolve(dirname);
 			Path file = dir.resolve(fileNames);
 
 			OutputStream os = outputStreamFactory.create(file.toFile());
@@ -159,8 +133,8 @@ public class MapWaysToTreeUsingArray extends
 		NodeProgress progress = new NodeProgress();
 		progress.printTimed(1000);
 
-		while (iterator.hasNext()) {
-			EntityContainer container = iterator.next();
+		while (wayIterator.hasNext()) {
+			EntityContainer container = wayIterator.next();
 			if (container.getType() != EntityType.Way) {
 				continue;
 			}
@@ -202,7 +176,6 @@ public class MapWaysToTreeUsingArray extends
 		System.out.println("none: " + nNone);
 		System.out.println("multiple: " + nMultiple);
 
-		input.close();
 		array.close();
 
 		outputNone.getOsmOutput().complete();
---
+++ extra/src/main/java/de/topobyte/osm4j/extra/executables/MapWaysToTreeUsingArray.java
@@ -0,0 +1,92 @@
+// Copyright 2015 Sebastian Kuerten
+//
+// This file is part of osm4j.
+//
+// osm4j is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osm4j is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with osm4j. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.osm4j.extra.executables;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import de.topobyte.osm4j.core.access.OsmIterator;
+import de.topobyte.osm4j.extra.datatree.ways.WaysToTreeMapperUsingArray;
+import de.topobyte.osm4j.utils.AbstractExecutableSingleInputStreamOutput;
+import de.topobyte.utilities.apache.commons.cli.OptionHelper;
+
+public class MapWaysToTreeUsingArray extends
+		AbstractExecutableSingleInputStreamOutput
+{
+
+	private static final String OPTION_FILE_NAMES = "filenames";
+	private static final String OPTION_TREE = "tree";
+	private static final String OPTION_NODE_ARRAY = "node_array";
+
+	@Override
+	protected String getHelpMessage()
+	{
+		return MapWaysToTreeUsingArray.class.getSimpleName() + " [options]";
+	}
+
+	public static void main(String[] args) throws IOException
+	{
+		MapWaysToTreeUsingArray task = new MapWaysToTreeUsingArray();
+
+		task.setup(args);
+
+		task.init();
+
+		task.execute();
+
+		task.finish();
+	}
+
+	private String pathTree;
+	private String pathNodeArray;
+
+	private String fileNames;
+
+	public MapWaysToTreeUsingArray()
+	{
+		// @formatter:off
+		OptionHelper.add(options, OPTION_FILE_NAMES, true, true, "names of the data files to create");
+		OptionHelper.add(options, OPTION_TREE, true, true, "directory to store output in");
+		OptionHelper.add(options, OPTION_NODE_ARRAY, true, true, "a path to a node array");
+		// @formatter:on
+	}
+
+	@Override
+	protected void setup(String[] args)
+	{
+		super.setup(args);
+
+		fileNames = line.getOptionValue(OPTION_FILE_NAMES);
+
+		pathTree = line.getOptionValue(OPTION_TREE);
+		pathNodeArray = line.getOptionValue(OPTION_NODE_ARRAY);
+	}
+
+	public void execute() throws IOException
+	{
+		OsmIterator iterator = createIterator();
+
+		WaysToTreeMapperUsingArray mapper = new WaysToTreeMapperUsingArray(
+				iterator, Paths.get(pathTree), fileNames,
+				Paths.get(pathNodeArray), outputFormat, pbfConfig, tboConfig,
+				writeMetadata);
+
+		mapper.execute();
+	}
+
+}
