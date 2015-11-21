20c9279cc0eabad55381bd3fc0c1aa0b695495a8
Sebastian Kürten <sebastian@topobyte.de>
2015-11-21 14:03:39 +0100

    Moved NodeArray executables

--- extra/src/main/java/de/topobyte/osm4j/extra/executables/NodeArrayInfo.java
+++ extra/src/main/java/de/topobyte/osm4j/extra/executables/NodeArrayInfo.java
@@ -15,23 +15,27 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with osm4j. If not, see <http://www.gnu.org/licenses/>.
 
-package de.topobyte.osm4j.extra.nodearray;
+package de.topobyte.osm4j.extra.executables;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import de.topobyte.osm4j.core.model.iface.OsmNode;
+import de.topobyte.osm4j.extra.nodearray.NodeArray;
+import de.topobyte.osm4j.extra.nodearray.NodeArrayDouble;
+import de.topobyte.osm4j.extra.nodearray.NodeArrayFloat;
+import de.topobyte.osm4j.extra.nodearray.NodeArrayInteger;
+import de.topobyte.osm4j.extra.nodearray.NodeArrayShort;
+import de.topobyte.osm4j.extra.nodearray.NodeArrayType;
 import de.topobyte.osm4j.utils.AbstractExecutable;
 import de.topobyte.utilities.apache.commons.cli.OptionHelper;
 
-public class NodeArrayShow extends AbstractExecutable
+public class NodeArrayInfo extends AbstractExecutable
 {
 
 	private static final String OPTION_INPUT = "input";
 	private static final String OPTION_TYPE = "type";
-	private static final String OPTION_ID = "id";
 
 	private static Map<String, NodeArrayType> typeMap = new HashMap<>();
 
@@ -47,12 +51,12 @@ public class NodeArrayShow extends AbstractExecutable
 	@Override
 	protected String getHelpMessage()
 	{
-		return NodeArrayShow.class.getSimpleName() + " [options]";
+		return NodeArrayInfo.class.getSimpleName() + " [options]";
 	}
 
 	public static void main(String[] args) throws IOException
 	{
-		NodeArrayShow task = new NodeArrayShow();
+		NodeArrayInfo task = new NodeArrayInfo();
 
 		task.setup(args);
 
@@ -63,17 +67,15 @@ public class NodeArrayShow extends AbstractExecutable
 
 	private String inputPath;
 	private NodeArrayType type;
-	private long id;
 
 	private File file;
 	private NodeArray array;
 
-	public NodeArrayShow()
+	public NodeArrayInfo()
 	{
 		// @formatter:off
 		OptionHelper.add(options, OPTION_INPUT, true, true, "input file");
 		OptionHelper.add(options, OPTION_TYPE, true, true, POSSIBLE_TYPES);
-		OptionHelper.add(options, OPTION_ID, true, true, "a node id");
 		// @formatter:on
 	}
 
@@ -91,9 +93,6 @@ public class NodeArrayShow extends AbstractExecutable
 					+ POSSIBLE_TYPES);
 			System.exit(1);
 		}
-
-		String argId = line.getOptionValue(OPTION_ID);
-		id = Long.parseLong(argId);
 	}
 
 	private void init() throws IOException
@@ -119,14 +118,10 @@ public class NodeArrayShow extends AbstractExecutable
 
 	private void execute() throws IOException
 	{
-		System.out.println("Queried id: " + id);
-		if (!array.contains(id)) {
-			System.out.println("Not found");
-		} else {
-			OsmNode node = array.get(id);
-			System.out.println("lon: " + node.getLongitude());
-			System.out.println("lat: " + node.getLatitude());
-		}
+		long length = file.length();
+		long entries = length / array.bytesPerRecord();
+
+		System.out.println("Size: " + entries);
 
 		array.close();
 	}
--- extra/src/main/java/de/topobyte/osm4j/extra/executables/NodeArrayShow.java
+++ extra/src/main/java/de/topobyte/osm4j/extra/executables/NodeArrayShow.java
@@ -15,21 +15,29 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with osm4j. If not, see <http://www.gnu.org/licenses/>.
 
-package de.topobyte.osm4j.extra.nodearray;
+package de.topobyte.osm4j.extra.executables;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import de.topobyte.osm4j.core.model.iface.OsmNode;
+import de.topobyte.osm4j.extra.nodearray.NodeArray;
+import de.topobyte.osm4j.extra.nodearray.NodeArrayDouble;
+import de.topobyte.osm4j.extra.nodearray.NodeArrayFloat;
+import de.topobyte.osm4j.extra.nodearray.NodeArrayInteger;
+import de.topobyte.osm4j.extra.nodearray.NodeArrayShort;
+import de.topobyte.osm4j.extra.nodearray.NodeArrayType;
 import de.topobyte.osm4j.utils.AbstractExecutable;
 import de.topobyte.utilities.apache.commons.cli.OptionHelper;
 
-public class NodeArrayInfo extends AbstractExecutable
+public class NodeArrayShow extends AbstractExecutable
 {
 
 	private static final String OPTION_INPUT = "input";
 	private static final String OPTION_TYPE = "type";
+	private static final String OPTION_ID = "id";
 
 	private static Map<String, NodeArrayType> typeMap = new HashMap<>();
 
@@ -45,12 +53,12 @@ public class NodeArrayInfo extends AbstractExecutable
 	@Override
 	protected String getHelpMessage()
 	{
-		return NodeArrayInfo.class.getSimpleName() + " [options]";
+		return NodeArrayShow.class.getSimpleName() + " [options]";
 	}
 
 	public static void main(String[] args) throws IOException
 	{
-		NodeArrayInfo task = new NodeArrayInfo();
+		NodeArrayShow task = new NodeArrayShow();
 
 		task.setup(args);
 
@@ -61,15 +69,17 @@ public class NodeArrayInfo extends AbstractExecutable
 
 	private String inputPath;
 	private NodeArrayType type;
+	private long id;
 
 	private File file;
 	private NodeArray array;
 
-	public NodeArrayInfo()
+	public NodeArrayShow()
 	{
 		// @formatter:off
 		OptionHelper.add(options, OPTION_INPUT, true, true, "input file");
 		OptionHelper.add(options, OPTION_TYPE, true, true, POSSIBLE_TYPES);
+		OptionHelper.add(options, OPTION_ID, true, true, "a node id");
 		// @formatter:on
 	}
 
@@ -87,6 +97,9 @@ public class NodeArrayInfo extends AbstractExecutable
 					+ POSSIBLE_TYPES);
 			System.exit(1);
 		}
+
+		String argId = line.getOptionValue(OPTION_ID);
+		id = Long.parseLong(argId);
 	}
 
 	private void init() throws IOException
@@ -112,10 +125,14 @@ public class NodeArrayInfo extends AbstractExecutable
 
 	private void execute() throws IOException
 	{
-		long length = file.length();
-		long entries = length / array.bytesPerRecord();
-
-		System.out.println("Size: " + entries);
+		System.out.println("Queried id: " + id);
+		if (!array.contains(id)) {
+			System.out.println("Not found");
+		} else {
+			OsmNode node = array.get(id);
+			System.out.println("lon: " + node.getLongitude());
+			System.out.println("lat: " + node.getLatitude());
+		}
 
 		array.close();
 	}
