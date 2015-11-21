7982745ff9ad74a95277946b45ab3a1203adfff8
Sebastian Kürten <sebastian@topobyte.de>
2015-11-21 11:55:06 +0100

    Moved relation distribution executables

--- extra/src/main/java/de/topobyte/osm4j/extra/executables/DistributeComplexRelations.java
+++ extra/src/main/java/de/topobyte/osm4j/extra/executables/DistributeComplexRelations.java
@@ -15,12 +15,13 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with osm4j. If not, see <http://www.gnu.org/licenses/>.
 
-package de.topobyte.osm4j.extra.relations;
+package de.topobyte.osm4j.extra.executables;
 
 import java.io.IOException;
 import java.nio.file.Paths;
 
 import de.topobyte.osm4j.core.access.OsmInputException;
+import de.topobyte.osm4j.extra.relations.ComplexRelationsDistributor;
 import de.topobyte.osm4j.utils.OsmOutputConfig;
 
 public class DistributeComplexRelations extends DistributeRelationsBase
--- extra/src/main/java/de/topobyte/osm4j/extra/executables/DistributeRelationsBase.java
+++ extra/src/main/java/de/topobyte/osm4j/extra/executables/DistributeRelationsBase.java
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with osm4j. If not, see <http://www.gnu.org/licenses/>.
 
-package de.topobyte.osm4j.extra.relations;
+package de.topobyte.osm4j.extra.executables;
 
 import java.nio.file.Path;
 import java.util.HashMap;
