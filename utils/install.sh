4089941ceb26399c03e75defeed00adc73f5ce5d
Sebastian Kürten <sebastian@topobyte.de>
2019-08-03 13:24:13 +0200

    Use launch-and-install plugin

---
+++ utils/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+
+pushd "$DIR" > /dev/null
+./gradlew clean installDist postInstallScript
+popd
+
+TARGET="$HOME/share/topobyte/osm4j-utils/osm4j-utils-snapshot"
+
+mkdir -p "$TARGET"
+rsync -av --delete "$DIR/cli/build/install/osm4j-utils/" "$TARGET"
+
+"$DIR"/cli/build/setup/post-install.sh
