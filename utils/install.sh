8342f83613f71ab02ed44ec30e42e9f8bcdcccfa
Sebastian Kürten <sebastian@topobyte.de>
2019-08-04 12:33:06 +0200

    Update launch-and-install-plugin

--- utils/install.sh
+++ utils/install.sh
@@ -5,12 +5,8 @@ set -e
 DIR=$(dirname $0)
 
 pushd "$DIR" > /dev/null
-./gradlew clean installDist postInstallScript
+./gradlew clean installDist setupScripts
 popd
 
-TARGET="$HOME/share/topobyte/osm4j-utils/osm4j-utils-snapshot"
-
-mkdir -p "$TARGET"
-rsync -av --delete "$DIR/cli/build/install/osm4j-utils/" "$TARGET"
-
+"$DIR"/cli/build/setup/install.sh
 "$DIR"/cli/build/setup/post-install.sh
